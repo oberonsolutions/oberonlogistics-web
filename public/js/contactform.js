8bf85202d7041e17cd7635723daaf0327a1886ad
Jim Phillips <jim@oberon.group>
2021-07-28 18:39:34 -0400

    Initial contact form

---
+++ public/js/contactform.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//grab a form
+const form = document.querySelector('#form-lead');
+
+//push on form submit
+if (form) {
+  form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    // CAPTCHA
+    grecaptcha.ready(function() {
+      grecaptcha.execute('6LcAKMgbAAAAAI1v-qN1hMhcgiv--3ka-sU5vsaZ', {action: 'submit'}).then(function(token) {
+          // Add your logic to submit to your backend server here.
+          alert token;
+      });
+    });
+/*
+    var mailsRef = firebase.database().ref('leads').push().set(
+      {
+        mail: input.value
+      }
+    );
+*/
+  })
+}
