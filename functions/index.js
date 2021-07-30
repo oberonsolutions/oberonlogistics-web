648e14f1b6ea5613082a305a07138de0c9688760
Jim Phillips <jim@oberon.group>
2021-07-30 16:33:32 -0400

    Contact Form updates Google Sheet

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,68 @@
 const functions = require("firebase-functions");
+const cors = require('cors')({origin: true});
+const axios = require('axios');
+const {google} = require('googleapis');
+const sheets = google.sheets("v4");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+async function updateSheets (data) {
+  // Get Secrets
+  const serviceAccount = functions.config().service_account;
+  const spreadsheetId = functions.config().sheets.id;
+
+  // Authenticate
+  const jwtClient = new google.auth.JWT({
+    email: serviceAccount.client_email,
+    key: serviceAccount.private_key,
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+  });
+  const jwtAuthPromise = jwtClient.authorize();
+  await jwtAuthPromise;
+
+  const aData = [
+    new Date().toLocaleDateString("en-US"),
+    'New',
+    data.name,
+    data.phone,
+    data.email,
+    data.memo
+  ];
+  const range = 'Leads!A2:F';
+
+  await sheets.spreadsheets.values.append(
+    {
+      auth: jwtClient,
+      spreadsheetId: spreadsheetId,
+      range: range,
+      valueInputOption: 'USER_ENTERED',
+      requestBody: { values: [ aData ] },
+    },
+    {}
+  );
+
+  return;
+};
+
+exports.processFormLead = functions.https.onRequest(async (req, res) => {
+  cors(req, res, () => {
+    // Verify Recaptcha Token
+    const secret = functions.config().recaptcha.key;
+    const token = req.body.token;
+    const formJSON = req.body.formJSON;
+
+    axios.get(`https://recaptcha.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`)
+    .then ((recaptcha) => {
+      if (recaptcha.data.success) {
+        if (recaptcha.data.score > 0.2) {
+          // Add Lead to Database Here
+          updateSheets(formJSON);
+        }
+        return res.status(200).send({ score: recaptcha.data.score });
+      } else {
+        return res.status(500).send(recaptcha);
+      }
+    })
+    .catch ((error) => {
+      return res.status(500).send(error);
+    });
+  });
+});
--- public/js/contactform.js
+++ public/js/contactform.js
@@ -8,13 +8,28 @@ form.addEventListener('submit', handleFormSubmit);
 function handleFormSubmit(event) {
   event.preventDefault();
 
+  const recaptchaKey = '6LcAKMgbAAAAAI1v-qN1hMhcgiv--3ka-sU5vsaZ';
+  const backendURL = 'https://us-central1-oberonlogistics-web.cloudfunctions.net/processFormLead';
+
   const data = new FormData(event.target);
   const formJSON = Object.fromEntries(data.entries());
-  const results = JSON.stringify(formJSON);
 
-  grecaptcha.ready(function() {
-    grecaptcha.execute('6LcAKMgbAAAAAI1v-qN1hMhcgiv--3ka-sU5vsaZ', {action: 'submit'}).then(function(token) {
-        // Add your logic to submit to your backend server here.
+  grecaptcha.ready(() => {
+    grecaptcha.execute(recaptchaKey, {action: 'submit'})
+    .then((token) => {
+      axios.post(backendURL, { token: token, formJSON: formJSON })
+      .then((response) => {
+        const score = response.data.score;
+ 
+        //Take action here based on score.
+        if (score > 0.2) {
+          const submit = document.querySelector("#form-lead button[type='submit']");
+          submit.innerHTML = 'Sent!';
+        }
+      })
+      .catch((error) => {
+        console.warn("error: ", error);
+      });
     });
   });
 }
