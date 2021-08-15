a3d2d6e043279b396810051db06988d6337494a5
Jim Phillips <jim@oberon.group>
2021-08-15 16:46:33 -0400

    Use new back-end for leads

--- public/js/contactform.js
+++ public/js/contactform.js
@@ -8,28 +8,17 @@ form.addEventListener('submit', handleFormSubmit);
 function handleFormSubmit(event) {
   event.preventDefault();
 
-  const recaptchaKey = '6LcAKMgbAAAAAI1v-qN1hMhcgiv--3ka-sU5vsaZ';
-  const backendURL = 'https://us-central1-oberonlogistics-web.cloudfunctions.net/processFormLead';
+  const backendURL = 'https://us-central1-oberonlogistics-2a5d9.cloudfunctions.net/leads';
 
   const data = new FormData(event.target);
   const formJSON = Object.fromEntries(data.entries());
 
-  grecaptcha.ready(() => {
-    grecaptcha.execute(recaptchaKey, {action: 'submit'})
-    .then((token) => {
-      axios.post(backendURL, { token: token, formJSON: formJSON })
-      .then((response) => {
-        const score = response.data.score;
- 
-        //Take action here based on score.
-        if (score > 0.2) {
-          const submit = document.querySelector("#form-lead button[type='submit']");
-          submit.innerHTML = 'Sent!';
-        }
-      })
-      .catch((error) => {
-        console.warn("error: ", error);
-      });
+  axios.post(backendURL, formJSON)
+    .then((response) => {
+      const submit = document.querySelector("#form-lead button[type='submit']");
+      submit.innerHTML = 'Sent!';
+    })
+    .catch((error) => {
+      console.warn("error: ", error);
     });
-  });
 }
